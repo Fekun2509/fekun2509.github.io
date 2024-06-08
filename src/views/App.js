2741827c714ed30149b45e1bde500dcaccad9a48
Fekun2509 <manh.hoang.kog@gmail.com>
2024-06-09 00:22:18 +1000

    React router

--- src/views/App.js
+++ src/views/App.js
@@ -4,23 +4,46 @@ import Mycomponent from './Example/Mycomponent';
 import ListTodo from './Todos/ListTodo';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Nav from './Navigation/Nav';
+import Home from './Example/Home';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Simple TODO Apps with React.js
-        </p>
+    <BrowserRouter>
+      <div className="App">
 
-        {/* <Mycomponent /> */}
-        <ListTodo />
+        <header className="App-header">
+          <Nav />
+          <img src={logo} className="App-logo" alt="logo" />
 
-      </header>
 
-      <ToastContainer />
-    </div>
+
+
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/todo">
+              <ListTodo />
+            </Route>
+            <Route path="/about">
+              <Mycomponent />
+            </Route>
+          </Switch>
+
+        </header>
+
+        <ToastContainer />
+      </div>
+    </BrowserRouter>
   );
 }
 
